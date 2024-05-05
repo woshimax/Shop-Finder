82549a6d9ab7dae94a0abc67b9a1cf83df1441b5
woshimax <1571421941@qq.com>
2024-05-05 21:57:56 +0800

    加上check锁是不是该线程的判断，防止业务阻塞，导致在该线程超时释放后，还去释放一次（把别的线程的锁释放了）
    该判断标准：通过uuid+线程id来标识
    问题：在check之后如果发生阻塞解决不了——这个是业务之外的阻塞，比如垃圾回收机制导致整个项目运行受阻

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -2,12 +2,14 @@ package com.hmdp.utils;
 
 import org.springframework.data.redis.core.StringRedisTemplate;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
     private String name;//锁的name由外部传入，不写死，共给不同业务用
     private StringRedisTemplate stringRedisTemplate;
     private final static String KEY_PREFIX = "lock:";
+    private final static String ID_PREFIX = UUID.randomUUID().toString() + "-";
 
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
@@ -19,13 +21,21 @@ public class SimpleRedisLock implements ILock{
     public boolean tryLock(long timeoutSec) {
 
         //锁的key是代表业务名，value则是当前线程的特征属性（线程唯一）
-        String threadName = Thread.currentThread().getName();
+        String threadName = ID_PREFIX + Thread.currentThread().getId();
         Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(KEY_PREFIX+name, threadName, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);//防止拆箱时出现空指针——null的封装类拆箱会变成空指针
     }
 
     @Override
     public void unLock() {
-        stringRedisTemplate.delete(KEY_PREFIX+name);
+        //判断当前线程的标识和锁里存的线程标识是否一致——一致代表是当前线程的锁
+        //解决一个问题：防止其他线程释放当前线程的锁
+        //问题场景：比如一个线程阻塞，业务未完成锁自动释放，之后别的线程创建锁被我这个阻塞完成的线程释放了
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        if(threadId.equals(id)){
+            stringRedisTemplate.delete(KEY_PREFIX+name);
+        }
+
     }
 }
