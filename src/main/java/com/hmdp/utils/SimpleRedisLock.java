eb7d9d119e01bcaf7f91976054db80f210c2e18d
woshimax <1571421941@qq.com>
2024-05-06 09:00:09 +0800

    使用lua脚本，使我们的判断和释放锁的操作具有原子性

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,7 +1,11 @@
 package com.hmdp.utils;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -10,6 +14,14 @@ public class SimpleRedisLock implements ILock{
     private StringRedisTemplate stringRedisTemplate;
     private final static String KEY_PREFIX = "lock:";
     private final static String ID_PREFIX = UUID.randomUUID().toString() + "-";
+    private final static DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    //在静态代码块中new，只需要加载一次资源，不需要每次unlock都加载资源造成性能浪费
+    static{
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
@@ -28,14 +40,28 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unLock() {
-        //判断当前线程的标识和锁里存的线程标识是否一致——一致代表是当前线程的锁
-        //解决一个问题：防止其他线程释放当前线程的锁
-        //问题场景：比如一个线程阻塞，业务未完成锁自动释放，之后别的线程创建锁被我这个阻塞完成的线程释放了
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        if(threadId.equals(id)){
-            stringRedisTemplate.delete(KEY_PREFIX+name);
-        }
+        //调用lua脚本——一批redis操作具有原子性
 
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId()
+                );
+        //一行代码解决——保证原子性
+        //区别与下面都判断和delete分成两行，可能中间出现阻塞而出现锁误删
     }
+
+
+    //    @Override
+//    public void unLock() {
+//        //判断当前线程的标识和锁里存的线程标识是否一致——一致代表是当前线程的锁
+//        //解决一个问题：防止其他线程释放当前线程的锁
+//        //问题场景：比如一个线程阻塞，业务未完成锁自动释放，之后别的线程创建锁被我这个阻塞完成的线程释放了
+//        String threadId = ID_PREFIX + Thread.currentThread().getId();
+//        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+//        if(threadId.equals(id)){
+//            stringRedisTemplate.delete(KEY_PREFIX+name);
+//        }
+//
+//    }
 }
